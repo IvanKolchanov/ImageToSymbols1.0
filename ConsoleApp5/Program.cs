d51e928410d22e61d8fa44de719d8f078ad153d4
w1ano <83294629+w1ano@users.noreply.github.com>
2023-09-28 17:37:00 -0500

    Added bestFit function, which creates an image with best resolution for a given font automatically

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,28 +7,41 @@ namespace ImageToSymbols
     {
         private static int height;
         private static int width;
+        private static bool bestFit = true;
         private static double coefficient = 2;
         private static bool colorInversion = false;
-
+        private static char[] gradient = new char[] { ' ', '.', ':', '!', '/', 'r', '(', 'l', '1', 'Z', '4', 'H', '9', 'W', '8', '$', '@' }; //add '\u25A0' to add the whitebox into the list
+        //char[] gradient = new char[] { '.', ':', '~', '!', '=', 'v', '7', 'I', '/', 'r', '(', 'l', '1', 'Z', '4', 'H', '9', 'W', '8', '$', '@' };
         private static void normalizeImage(Bitmap image)
         {
-            char[] gradient = new char[] {' ', '.', ':', '!', '/', 'r', '(', 'l', '1', 'Z', '4', 'H', '9', 'W', '8', '$', '@' };
-            //char[] gradient = new char[] { '.', ':', '~', '!', '=', 'v', '7', 'I', '/', 'r', '(', 'l', '1', 'Z', '4', 'H', '9', 'W', '8', '$', '@' };
             long oneColor = 255 * 255 * 255 / gradient.Length;
-
-            if (image.Width == height && image.Width == width)
+            int wAspect = 1, hAspect = 1;
+            Console.BufferHeight = Console.LargestWindowHeight + 10;
+            int wX = -1, wY = -1, hX = -1, hY = -1;
+            if (bestFit)
             {
-                for (int j = 0; j < height; j++)
-                {
-                    for (int i = 0; i < width; i++)
-                    {
-
-                        Color pixelColor = image.GetPixel(i, j);
-                        int pixelColorBrightness = pixelColor.R * pixelColor.B * pixelColor.G;
-                        int gradientNum = (int)Math.Min(pixelColorBrightness / oneColor, gradient.Length - 1);
-                        Console.Write(gradient[gradientNum] + "" + gradient[gradientNum]);
-                    }
+                double imageAspect = Convert.ToDouble(image.Height) / Convert.ToDouble(image.Width);
+                if (imageAspect > 1) {
+                    image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    imageAspect = 1.0 / imageAspect;
                 }
+                if (Console.LargestWindowWidth / 2.0 >= Console.LargestWindowHeight / imageAspect)
+                {
+                    height = Console.LargestWindowHeight;
+                    width = (int)(height / imageAspect);
+                    
+                }else
+                {
+                    width = (int)(Console.LargestWindowWidth / 2.0);
+                    height = (int)(width * imageAspect / 2.0);
+                }
+                Console.SetWindowSize(width * 2, height);
+                Console.BufferWidth = Console.WindowWidth;
+                wAspect = image.Width / width;
+                hAspect = image.Height / height;
+                wY = image.Width - wAspect * width; wX = width - wY;
+                hY = image.Height - hAspect * height; hX = height - hY;
+                Console.WriteLine(width + " " + height + " " + wAspect + " " + hAspect + " " + wX + " " + hX);
             }
             else
             {
@@ -38,46 +51,54 @@ namespace ImageToSymbols
                 if (aspect > currentAscpect)
                 {
                     h = (int)(w / aspect);
-                }else
+                }
+                else
                 {
                     w = (int)(h * aspect);
                 }
-                int wAspect = w / width;
-                int hAspect = h / height;
-                for (int j = 0; j < height; j++)
+                wAspect = w / width;
+                hAspect = h / height;
+            }
+            int imageCordX = 0, imageCordY = 0;
+            for (int j = 0; j < height; j++)
+            {
+                if (bestFit && j >= hX) hAspect++;
+                for (int i = 0; i < width; i++)
                 {
-                    for (int i = 0; i < width; i++)
+                    long sum = 0;
+                    if (bestFit && i >= wX) wAspect++;
+                    for (int z = imageCordY; z < imageCordY + hAspect; z++)
                     {
-                        long sum = 0;
-                        for (int z = j * hAspect; z < hAspect * (j + 1); z++)
+                        for (int t = imageCordX; t < imageCordX + wAspect; t++)
                         {
-                            
-                            for (int t = i * wAspect; t < wAspect * (i+1); t++)
-                            {
-                                Color pixel = image.GetPixel(t, z);
-                                int pixelBrightness = (int)coefficient * pixel.R * pixel.G * pixel.B;
-                                sum += pixelBrightness;
-                            }
-                        }
-                        sum = sum / (wAspect * hAspect);
-                        int gradientNum = (int)Math.Max(0, Math.Min(sum / oneColor, gradient.Length - 1));
-                        if (!colorInversion)
-                        {
-                            Console.Write(gradient[gradientNum] + "" + gradient[gradientNum]);
-                        }
-                        else
-                        {
-                            Console.Write(gradient[gradient.Length - 1 - gradientNum] + "" + gradient[gradient.Length - 1 - gradientNum]);
+                            Color pixel = image.GetPixel(t, z);
+                            int pixelBrightness = (int)(coefficient * (double)pixel.R * pixel.G * pixel.B);
+                            sum += pixelBrightness;
                         }
                     }
+                    sum = sum / (wAspect * hAspect);
+                    int gradientNum = (int)Math.Max(0, Math.Min(sum / oneColor, gradient.Length - 1));
+                    if (!colorInversion)
+                    {
+                        Console.Write(gradient[gradientNum] + "" + gradient[gradientNum]);
+                    }
+                    else
+                    {
+                        Console.Write(gradient[gradient.Length - 1 - gradientNum] + "" + gradient[gradient.Length - 1 - gradientNum]);
+                    }
+                    imageCordX += wAspect;
+                    if (bestFit && i >= wX) wAspect--;
                 }
-            }     
+                imageCordX = 0;
+                imageCordY += hAspect;
+                if (bestFit && j >= hX) hAspect--;
+            }
         }
 
         static void Main(string[] args)
         {
             Console.Title = "Image to symbols";
-            Console.SetWindowSize(224, 63);
+            //Console.SetWindowSize(Math.Min(270, Console.LargestWindowWidth), Math.Min(135, Console.LargestWindowHeight));
             height = Console.WindowHeight;
             width = Console.WindowWidth / 2;
             while (true)
@@ -87,13 +108,14 @@ namespace ImageToSymbols
                 Console.WriteLine("2. Change the coefficient of transforming");
                 Console.WriteLine("3. Change window's parameters");
                 Console.WriteLine("4. Change color inversion");
+                Console.WriteLine("5. Change best fit");
                 ConsoleKey input = Console.ReadKey(true).Key;
                 switch (input)
                 {
                     case ConsoleKey.D1:
                         Console.Clear();
                         Console.WriteLine("Enter the full path to the photo you want to transform");
-                        String path = Console.ReadLine();
+                        String path = Console.ReadLine().Replace("\"", "");
                         try
                         {
                             Bitmap image = new Bitmap(@path, true);
@@ -102,21 +124,21 @@ namespace ImageToSymbols
                             {
                                 Console.Clear();
                                 normalizeImage(image);
-                                Console.WriteLine("Press A if you want to change the coefficient");
-                                Console.WriteLine("Press ArrowRight or ArrowLeft to turn the picture 90 degrees in the chosen direction");
+                                Console.WriteLine("Press F if you want to change the coefficient");
+                                if (!bestFit) Console.WriteLine("Press ArrowRight or ArrowLeft to turn the picture 90 degrees in the chosen direction");
                                 ConsoleKey key = Console.ReadKey(true).Key;
                                 switch (key)
                                 {
-                                    case ConsoleKey.A:
+                                    case ConsoleKey.F:
                                         Console.Clear();
                                         Console.WriteLine("Enter the coefficient");
-                                        coefficient = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+                                        coefficient = Convert.ToDouble(Console.ReadLine().Replace(',', '.'));
                                         break;
                                     case ConsoleKey.LeftArrow:
-                                        image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                                        if (!bestFit) image.RotateFlip(RotateFlipType.Rotate270FlipNone);
                                         break;
                                     case ConsoleKey.RightArrow:
-                                        image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                                        if (!bestFit) image.RotateFlip(RotateFlipType.Rotate90FlipNone);
                                         break;
                                     default:
                                         check = false;
@@ -167,12 +189,21 @@ namespace ImageToSymbols
                         {
                             Console.WriteLine("Color inversion is off");
                         }
-                        Console.WriteLine("\n" + "If you want to leave it that way, press ESC");
+                        Console.WriteLine("\nIf you want to leave it that way, press ESC");
                         Console.WriteLine("If not, press any button");
-                        ConsoleKey input1 = Console.ReadKey(true).Key;
-                        if (input1 == ConsoleKey.Escape) break;
+                        if (Console.ReadKey(true).Key == ConsoleKey.Escape) break;
                         colorInversion = !colorInversion;
                         break;
+                    case ConsoleKey.D5:
+                        Console.Clear();
+                        if (bestFit) Console.WriteLine("Best fit is turned on");
+                        else Console.WriteLine("Best fit is turned off");
+                        Console.WriteLine("\nIf you want to leave it that way, press ESC");
+                        Console.WriteLine("If not, press any button");
+                        if(Console.ReadKey(true).Key == ConsoleKey.Escape) break;
+                        bestFit = !bestFit;
+                        break;
+
                 }
                 Console.Clear();
             }
